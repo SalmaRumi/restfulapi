903db738870f892e4106e8289c3b0e898eb527e0
SalmaRumi <38862197+SalmaRumi@users.noreply.github.com>
2019-11-27 12:32:03 +0600

    port1

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get('/', (req, res) => {
 
 // listen for requests
 require('./app/routes/note.routes.js')(app);
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is listening on port 3000");
 });
